47ccae1ab1d843a975967c65331007bb596fd4b2
Kjartan Olason <olasonk@gmail.com>
2022-05-25 17:47:12 -0300

    Adding test spritesheets for a lot of formats

---
+++ src/test/resources/sheets/kwik.lua
@@ -0,0 +1,15 @@
+-- created with TexturePacker (http://www.texturepacker.com)
+           frames = {
+             
+               { x=293, y=117, width=16, height=16 }, -- icon_16
+               { x=259, y=117, width=32, height=32 }, -- icon_32
+               { x=259, y=67, width=48, height=48 }, -- icon_48
+               { x=259, y=1, width=64, height=64 }, -- icon_64
+               { x=229, y=259, width=72, height=72 }, -- icon_72
+               { x=131, y=259, width=96, height=96 }, -- icon_96
+               { x=1, y=259, width=128, height=128 }, -- icon_128
+               { x=1, y=1, width=256, height=256 }, -- icon_256
+           },
+    
+           sheetContentWidth = 324,
+           sheetContentHeight = 388
